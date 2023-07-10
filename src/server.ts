5a73851ef8357e8363b5a584f75ddcafd8e4b699
Mark <mark841030@gmail.com>
2023-07-10 15:57:47 +0800

    add comments to specify the usage of files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
-// can be access by http://localhost:3000/ after running npx ts-node src/server.ts
+// To run the typescript file:
+// way 1: 
+// can be access by http://localhost:3000/ after running $ npx ts-node src/server.ts
 // The command npx ts-node is a shortcut to run typescript code without the need to compile into javascript
-const express = require('express')
-const app = express()
+// (preferred) way 2:
+// run with nodemon in 'watch' mode, so that every time the files are changed, the changes are applied
+// first, set up "start:dev": "nodemon src/server.ts" in package.json
+// then run $ npm run start:dev, which will start with the 'start:dev' script
+// way 3:
+// transpile ts into js by running $ tsc src/server.ts
+// and then run the js file with $ node src/server.js
+//============================================================================================================
+// To stop, try Ctrl+C if still running in command line, or
+// check process id with $ lsof -i :3000
+// and kill with $ kill -9 <pid>
+
+import express, { Request, Response } from "express";
+import { json } from "body-parser";
+// const express = require('express')
+const app = express() // create an express server
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(json()) // body parser middleware
+
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
