d3df8370347178f19d023fd4b33d385d0c4985e1
Mark <mark841030@gmail.com>
2023-07-10 19:33:15 +0800

    change file structure

--- src/controller.ts
+++
@@ -1,7 +0,0 @@
-import express, { Request, Response } from "express";
-
-const controllerFunction = (req: Request, res: Response) => {
-    res.send('Hello World!')
-};
-
-export default controllerFunction;
